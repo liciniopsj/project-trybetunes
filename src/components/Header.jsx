85e58e8603d520095d4b81328b22265c0f7596ab
licinio.psj@gmail.com <110546077+liciniopsj@users.noreply.github.com>
2022-11-04 20:58:34 -0300

    Requisite 7

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -36,12 +36,11 @@ class Header extends Component {
     const { showLoadingScreen, userCredentials: { name } } = this.state;
     return (
       <header data-testid="header-component">
-        Header
+        {showLoadingScreen
+          ? <Loading /> : <span data-testid="header-user-name">{ `${name}` }</span>}
         <Link data-testid="link-to-search" to="/search">Search</Link>
         <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
         <Link data-testid="link-to-profile" to="/profile">Profile</Link>
-        {showLoadingScreen
-          ? <Loading /> : <p data-testid="header-user-name">{ `${name}` }</p>}
       </header>
     );
   }
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,15 +1,49 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import getMusics from '../services/musicsAPI';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
 
 class Album extends Component {
+  state = {
+    getMusicsResults: [],
+    artistInfo: '',
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+    const { id } = match.params;
+    const getMusicsResults = await getMusics(id);
+    const artistInfo = getMusicsResults[0];
+    // getMusicsResults.shift();
+    // console.log(getMusicsResults);
+    // console.log(artistInfo);
+    this.setState({
+      getMusicsResults,
+      artistInfo,
+    });
+  }
+
   render() {
     const { match } = this.props;
     const { id } = match.params;
+    const { getMusicsResults, artistInfo } = this.state;
+    const { artistName, collectionName } = artistInfo;
+    const musicCardList = getMusicsResults.slice(1);
     return (
       <div data-testid="page-album">
         <Header />
-        { `Album ${id}` }
+        <p>{ `Album ${id}` }</p>
+        <div>
+          <img src={ artistInfo.artworkUrl100 } alt="Album Cover" />
+          <h3 data-testid="artist-name">{artistName}</h3>
+          <h4 data-testid="album-name">{collectionName}</h4>
+        </div>
+        {musicCardList.map((track) => (
+          <div key={ track.trackId }>
+            <MusicCard trackName={ track.trackName } previewUrl={ track.previewUrl } />
+          </div>
+        ))}
       </div>
     );
   }
